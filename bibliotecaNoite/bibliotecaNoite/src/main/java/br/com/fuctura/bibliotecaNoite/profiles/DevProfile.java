c73bddd284945b630086ed212b1fb7d18f97788d
Mike <matthedan@gmail.com>
2025-07-23 19:59:25 -0300

    -dev configurado com postgres in cloud

--- bibliotecaNoite/bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecaNoite/profiles/DevProfile.java
+++ bibliotecaNoite/bibliotecaNoite/src/main/java/br/com/fuctura/bibliotecaNoite/profiles/DevProfile.java
@@ -1,6 +1,22 @@
 package br.com.fuctura.bibliotecaNoite.profiles;
 
+import br.com.fuctura.bibliotecaNoite.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
 public class DevProfile {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instaciaDB() {
+        this.dbService.instanciaDB();
+    }
 
 
 }
